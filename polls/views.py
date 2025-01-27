082a75d2cc6e35a9cd1013d5888a1d9d6ab01b3a
Maxidonx <johnmaxwell927@gmail.com>
2025-01-27 05:19:57 -0600

    created a template folder and file for my index.html

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
-
+from .models import Question
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ",".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def details(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results ofquestion %s."
+    return HttpResponse(response % question_id)
+
+def votes(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
